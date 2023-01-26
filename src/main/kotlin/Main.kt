b7dcc2ffe21f2c1b09c3eba35d3e4a26492e3a72
Eugene Otieno <otienoeo@gmail.com>
2023-01-26 09:24:55 +0300

    Multiple sum in with exit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,19 @@
 fun main() {
-    var sum = 0
-    val numbers = readln().split("\\s+".toRegex())
-    for (number in numbers) {
-        sum += number.toInt()
+    var calculate = true
+    while (calculate) {
+        val input = readln()
+        if (input.isNotBlank()) {
+            if (input == "/exit") {
+                println("Bye!")
+                calculate = false
+            } else {
+                var sum = 0
+                val numbers = input.split("\\s+".toRegex())
+                for (number in numbers) {
+                    sum += number.toInt()
+                }
+                println(sum)
+            }
+        }
     }
-
-    println(sum)
 }
